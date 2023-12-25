4a37693843e50228b05712ffc9848445f09580aa
CodexScript <67811878+CodexScript@users.noreply.github.com>
2023-12-24 23:34:59 -0500

    Update APIServer.ts

--- src/server/APIServer.ts
+++ src/server/APIServer.ts
@@ -24,14 +24,27 @@ function kickContinous(durationMillis: number, discordBot: Bot, id: string): Pro
             if (Date.now() - startTime >= durationMillis) {
                 resolve();
             } else {
+                try {
                 const guild = discordBot.guilds.cache.get(discordBot.config.mainGuildID);
+                if(!guild) {
+                    resolve();
+                }
+
                 const member = await guild?.members.fetch(id);
+
+                if (!member) {
+                    resolve();
+                }
+
                 if (member) {
                     if (member.voice.channel) {
                         await member.voice.setChannel(null);
                     }
                 }
                 setTimeout(checkTime, 100);
+            } catch (e) {
+                console.log(e);
+                resolve();
             }
         }
 
