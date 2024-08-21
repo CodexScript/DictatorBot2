143af0921154a59bc4b9820eb4f02d0645eb34c0
CodexScript <67811878+CodexScript@users.noreply.github.com>
2024-08-21 01:32:16 -0400

    See balance for other users

--- src/commands/util/Balance.ts
+++ src/commands/util/Balance.ts
@@ -1,12 +1,27 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { ChatInputCommandInteraction } from 'discord.js';
-import { formatCurrency, getBalance, setBalance } from '../../util/BalanceUtil.js';
+import { formatCurrency, getBalance } from '../../util/BalanceUtil.js';
 
 
-export const data = new SlashCommandBuilder().setName('bal').setDescription('Shows your current balance.');
+export const data = new SlashCommandBuilder().setName('bal')
+    .setDescription('Shows your current balance.')
+    .addUserOption(option => 
+        option.setName('user')
+            .setDescription('The user to check the balance for. Defaults to you.')
+            .setRequired(false));
 
 export async function execute(interaction: ChatInputCommandInteraction): Promise<void> {
-    const result = await getBalance(interaction.client.sql, interaction.user.id);
+    let target = interaction.options.getUser('user');
 
-    await interaction.reply({ content: `Your balance: **${formatCurrency(result.balance_cents / 100)}**\nMost gained in one run: **${formatCurrency(result.most_gained / 100)}**\nMost lost in one run: **${formatCurrency(result.most_lost / 100)}**`, ephemeral: true });
+    if (!target) {
+        target = interaction.user;
+    }
+
+    const result = await getBalance(interaction.client.sql, target.id);
+
+    if (target) {
+        await interaction.reply({ content: `${target.displayName} balance: **${formatCurrency(result.balance_cents / 100)}**\nMost gained in one run: **${formatCurrency(result.most_gained / 100)}**\nMost lost in one run: **${formatCurrency(result.most_lost / 100)}**`, ephemeral: true });
+    } else {
+        await interaction.reply({ content: `Your balance: **${formatCurrency(result.balance_cents / 100)}**\nMost gained in one run: **${formatCurrency(result.most_gained / 100)}**\nMost lost in one run: **${formatCurrency(result.most_lost / 100)}**`, ephemeral: true });
+    }
 }
